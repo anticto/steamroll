dec96b05ee623a33041ad6efd132acd945cdbc97
alexei-anticto <alexei@anticto.com>
2015-02-23 11:17:54 +0100

    First version of the new 3D ui.

---
+++ Steamroll/Source/Steamroll/Private/Steamroll3DHud.cpp
@@ -0,0 +1,13 @@
+// Copyright Anticto Estudi Binari, SL. All rights reserved.
+
+#include "Steamroll.h"
+#include "Steamroll3DHud.h"
+
+
+ASteamroll3DHud::ASteamroll3DHud(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+
---
+++ Steamroll/Source/Steamroll/Public/Steamroll3DHud.h
@@ -0,0 +1,18 @@
+// Copyright Anticto Estudi Binari, SL. All rights reserved.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Steamroll3DHud.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEAMROLL_API ASteamroll3DHud : public AActor
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
